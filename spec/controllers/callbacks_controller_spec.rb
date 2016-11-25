39e4f15d18959800afb4d12c973695aabe667c72
Robert Steilberg <rsteilberg@gmail.com>
2016-11-25 18:02:48 -0500

    fix RSpec tests

--- spec/controllers/callbacks_controller_spec.rb
+++ spec/controllers/callbacks_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe CallbacksController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    it "always redirects back to users/show" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to("/users/show")
     end
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
+  describe "GET #authenticate" do
+    it "redirects to users/authenticate if user has not authenticated" do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to("/users/authenticate")
+    end
+
+    it "rescues and redirects to users/authenticate if access_token is invalid" do
+      get :show
+      session[:accessToken] = 'test'
+      expect(response).to redirect_to("/users/authenticate")
     end
   end
 
-  it 'returns nil if user has not authenticated' do
-    controller.authenticated?.should == nil
-  end
+  describe "authenticate check" do
+    it 'returns nil if user has not authenticated' do
+      controller.authenticated?.should == nil
+    end
 
-  it 'prompts user to authenticate if user has not authenticated' do
-    get :index
-    expect(response).to redirect_to("/github/authenticate")
+    it 'returns session[:accessToken] if user has authenticated' do
+      session[:accessToken] = 'test'
+      controller.authenticated?.should == session[:accessToken]
+    end
   end
 
 end
