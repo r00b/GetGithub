3d159c9041d2917551a11d487c07512f016ae3c4
Robert Steilberg <rsteilberg@gmail.com>
2016-11-25 00:20:10 -0500

    implement GitHub API callback handling

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,4 +1,12 @@
 class CallbacksController < ApplicationController
   def new
+    session_code = request.env['rack.request.query_hash']['code']
+    result = RestClient.post('https://github.com/login/oauth/access_token',
+    {:client_id => ENV['GITHUB_CLIENT_ID'],
+      :client_secret => ENV['GITHUB_CLIENT_SECRET'],
+      :code => session_code},
+      :accept => :json)
+    session[:accessToken] = JSON.parse(result)['access_token']
+    redirect_to "/users/show"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
 
+  def authenticated?
+    session[:accessToken]
+  end
+
+  def authenticate!
+    redirect_to "/users/authenticate"
+  end
+
   def show
+    if !authenticated?
+      authenticate!
+    else
+      client = Octokit::Client.new(:access_token => session[:accessToken])
+      @user = client.user
+    end
   end
 end
