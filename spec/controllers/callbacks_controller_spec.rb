6e419f2ba7c9de5ab48f675b4e2a1d899ec00f5e
Robert Steilberg <rsteilberg@gmail.com>
2016-11-25 23:14:29 -0500

    cleanup RSpec tests

--- spec/controllers/callbacks_controller_spec.rb
+++ spec/controllers/callbacks_controller_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe CallbacksController, type: :controller do
 
-  describe "GET #new" do
-    it "redirects back to users/show" do
+  describe 'GET #new' do
+    it "redirects back to users/show after API call" do
       get :new
-      expect(response).to redirect_to("/users/show")
+      expect(response).to redirect_to('/users/show')
     end
   end
-
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #authenticate" do
+  describe 'GET #authenticate' do
     context 'unauthenticated request' do
-      it "redirects to users/authenticate" do
+      it 'redirects to users/authenticate' do
         get :show
-        expect(response).to redirect_to("/users/authenticate")
+        expect(response).to redirect_to('/users/authenticate')
       end
     end
 
     context 'invalid access_token' do
-      it "rescues and redirects to users/authenticate" do
-        session[:current_access_token] = 'test'
+      it 'rescues and redirects to users/authenticate' do
+        session[:current_access_token] = 'invalid'
         get :show
-        expect(response).to redirect_to("/users/authenticate")
+        expect(response).to redirect_to('/users/authenticate')
       end
     end
   end
 
-  describe "authentication check" do
+  describe 'authentication check' do
     context 'authenticated request' do
       it 'returns session[:current_access_token] if user has authenticated' do
         session[:current_access_token] = 'test'
